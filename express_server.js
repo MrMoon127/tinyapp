5323c3ad3a0b960d77090de3be95c21f9cbfc458
Sasha Andia <sasha.andia@gmail.com>
2023-07-05 18:08:59 -0700

    started being able to submit things for post and recieve ok message

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const shortURLIDLength = 6;
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "Http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,6 +22,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -31,6 +40,20 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
+
+function generateRandomString() {
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < shortURLIDLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}

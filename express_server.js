99e4db5249ef80a6a937d81a2458bbce30d4f975
Sasha Andia <sasha.andia@gmail.com>
2023-07-07 13:40:09 -0700

    can now display username and logout button, logout button not initialized

--- express_server.js
+++ express_server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cookieParser = require("cookie-parser")
+
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -19,16 +22,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    username: req.cookies["username"],
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] 
+  };
   res.render("urls_show", templateVars);
 });
 
